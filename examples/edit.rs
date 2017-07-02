a3c6a6a9a364b941f8686e11381a13c356d38dc5
agatan <agatan039@gmail.com>
2017-07-02 18:32:22 +0900

    Fix editor example

--- examples/edit.rs
+++ examples/edit.rs
@@ -8,55 +8,70 @@ use unicode_width::UnicodeWidthChar;
 use festerm::{Festerm, Event};
 use festerm::keys::*;
 
-#[derive(Default)]
 struct Editor {
     before_cursor: VecDeque<char>,
     after_cursor: VecDeque<char>,
+    festerm: Festerm,
+    cursor: i32,
 }
 
 impl Editor {
+    fn new(fest: Festerm) -> Self {
+        Editor {
+            before_cursor: VecDeque::new(),
+            after_cursor: VecDeque::new(),
+            festerm: fest,
+            cursor: 0,
+        }
+    }
+
     fn insert(&mut self, ch: char) {
         self.before_cursor.push_back(ch);
+        let mut screen = self.festerm.lock_screen();
+        screen.put_char(self.cursor, 0, ch);
+        self.cursor += ch.width_cjk().unwrap_or(1) as i32;
+        let mut x = self.cursor;
+        for &ch in self.after_cursor.iter() {
+            screen.put_char(x, 0, ch);
+            x += ch.width_cjk().unwrap_or(1) as i32;
+        }
+        screen.move_cursor(self.cursor, 0);
     }
 
     fn backspace(&mut self) {
-        self.before_cursor.pop_back();
+        if let Some(ch) = self.before_cursor.pop_back() {
+            let mut screen = self.festerm.lock_screen();
+            self.cursor -= ch.width_cjk().unwrap_or(1) as i32;
+            let mut x = self.cursor;
+            for &ch in self.after_cursor.iter() {
+                screen.put_char(x, 0, ch);
+                x += ch.width_cjk().unwrap_or(1) as i32;
+            }
+            screen.put_char(x, 0, ' ');
+            screen.move_cursor(self.cursor, 0);
+        }
     }
 
     fn move_left(&mut self) {
         if let Some(ch) = self.before_cursor.pop_back() {
             self.after_cursor.push_front(ch);
+            self.cursor -= ch.width_cjk().unwrap_or(1) as i32;
+            self.festerm.lock_screen().move_cursor(self.cursor, 0);
         }
     }
 
     fn move_right(&mut self) {
         if let Some(ch) = self.after_cursor.pop_front() {
             self.before_cursor.push_back(ch);
+            self.cursor += ch.width_cjk().unwrap_or(1) as i32;
+            self.festerm.lock_screen().move_cursor(self.cursor, 0);
         }
-    }
-
-    fn show(&self, fest: &mut Festerm) {
-        let mut screen = fest.lock_screen();
-        screen.clear();
-        screen.print(0,
-                     0,
-                     self.before_cursor
-                         .iter()
-                         .chain(self.after_cursor.iter())
-                         .cloned()
-                         .collect::<String>()
-                         .as_str());
-        screen.move_cursor(self.before_cursor
-                               .iter()
-                               .map(|ch| ch.width_cjk().unwrap_or(1) as i32)
-                               .sum(),
-                           0);
     }
 }
 
 fn main() {
-    let mut editor = Editor::default();
-    let (mut fest, rx) = festerm::hold().unwrap();
+    let (fest, rx) = festerm::hold().unwrap();
+    let mut editor = Editor::new(fest);
 
     for ev in rx.iter() {
         match ev {
@@ -74,6 +89,5 @@ fn main() {
             }
             _ => {}
         }
-        editor.show(&mut fest);
     }
 }
