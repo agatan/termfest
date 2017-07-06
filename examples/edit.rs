6b8237dafa4c3d2a1654f39c5a09270ba3337e82
agatan <agatan039@gmail.com>
2017-07-06 14:48:57 +0900

    Fix edit example

--- examples/edit.rs
+++ examples/edit.rs
@@ -3,76 +3,59 @@ extern crate unicode_width;
 
 use std::collections::VecDeque;
 
-use termfest::{TermFest, Event, Cell, DisplayWidth};
+use termfest::{TermFest, Event, Cell, DisplayWidth, Attribute};
 use termfest::keys::*;
 
 struct Editor {
-    before_cursor: VecDeque<char>,
-    after_cursor: VecDeque<char>,
-    termfest: TermFest,
-    cursor: i32,
+    contents: String,
+    cursor: usize,
 }
 
 impl Editor {
-    fn new(fest: TermFest) -> Self {
+    fn new() -> Self {
         Editor {
-            before_cursor: VecDeque::new(),
-            after_cursor: VecDeque::new(),
-            termfest: fest,
+            contents: String::new(),
             cursor: 0,
         }
     }
 
     fn insert(&mut self, ch: char) {
-        self.before_cursor.push_back(ch);
-        let mut screen = self.termfest.lock();
-        screen.put_cell(self.cursor, 0, Cell::new(ch));
-        self.cursor += ch.display_width() as i32;
-        let mut x = self.cursor;
-        for &ch in self.after_cursor.iter() {
-            screen.put_cell(x, 0, Cell::new(ch));
-            x += ch.display_width() as i32;
-        }
-        screen.move_cursor(self.cursor, 0);
+        self.contents.insert(self.cursor, ch);
+        self.cursor += ch.len_utf8();
     }
 
     fn backspace(&mut self) {
-        if let Some(ch) = self.before_cursor.pop_back() {
-            let mut screen = self.termfest.lock();
-            self.cursor -= ch.display_width() as i32;
-            let mut x = self.cursor;
-            for &ch in self.after_cursor.iter() {
-                screen.put_cell(x, 0, Cell::new(ch));
-                x += ch.display_width() as i32;
-            }
-            screen.put_cell(x, 0, Cell::new(' '));
-            screen.put_cell(x + 1, 0, Cell::new(' '));
-            screen.move_cursor(self.cursor, 0);
+        if let Some(ch) = self.contents[..self.cursor].chars().rev().next() {
+            self.cursor -= ch.len_utf8();
+            self.contents.remove(self.cursor);
         }
     }
 
     fn move_left(&mut self) {
-        if let Some(ch) = self.before_cursor.pop_back() {
-            self.after_cursor.push_front(ch);
-            let mut screen = self.termfest.lock();
-            self.cursor -= ch.display_width() as i32;
-            screen.move_cursor(self.cursor, 0);
+        if let Some(ch) = self.contents[..self.cursor].chars().rev().next() {
+            self.cursor -= ch.len_utf8();
         }
     }
 
     fn move_right(&mut self) {
-        if let Some(ch) = self.after_cursor.pop_front() {
-            self.before_cursor.push_back(ch);
-            let mut screen = self.termfest.lock();
-            self.cursor += ch.display_width() as i32;
-            screen.move_cursor(self.cursor, 0);
+        if let Some(ch) = self.contents[self.cursor..].chars().next() {
+            self.cursor += ch.len_utf8();
         }
+    }
+
+    fn show(&self, fest: &TermFest) {
+        let mut screen = fest.lock();
+        screen.clear();
+        screen.print(0, 0, &self.contents, Attribute::default());
+        let cursor_x = self.contents[..self.cursor].display_width() as i32;
+        screen.move_cursor(cursor_x, 0);
     }
 }
 
 fn main() {
     let (fest, rx) = TermFest::hold().unwrap();
-    let mut editor = Editor::new(fest);
+    let mut editor = Editor::new();
+    editor.show(&fest);
 
     for ev in rx.iter() {
         match ev {
@@ -90,5 +73,6 @@ fn main() {
             }
             _ => {}
         }
+        editor.show(&fest);
     }
 }
